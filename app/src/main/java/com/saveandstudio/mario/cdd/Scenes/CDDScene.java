8e3cb70119154d0f96460764c40f6fdd08f6a582
Mario8664 <pooh3000@126.com>
2019-05-12 13:01:26 +0800

    场景编辑功能完成，渲染物体只消两步！！目前只能渲bitmap，以后支持重写draw函数，搞得像shader一样就厉害了

--- app/src/main/java/com/saveandstudio/mario/cdd/Scenes/CDDScene.java
+++ app/src/main/java/com/saveandstudio/mario/cdd/Scenes/CDDScene.java
@@ -1,5 +1,15 @@
 package com.saveandstudio.mario.cdd.Scenes;
 
+import com.saveandstudio.mario.cdd.GameBasic.GameObject;
+import com.saveandstudio.mario.cdd.GameBasic.Renderer;
+import com.saveandstudio.mario.cdd.GameBasic.Transform;
+import com.saveandstudio.mario.cdd.R;
+
 public class CDDScene {
 
+
+    public static void prePareScene(){
+        GameObject gameObject = new GameObject(new Transform());
+        gameObject.addComponent(new Renderer(R.mipmap.default_sprite));
+    }
 }
