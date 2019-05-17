b33a9155fcbfcb513b7f7eb1cb0ba22d0bfcb09e
Mario8664 <pooh3000@126.com>
2019-05-17 17:14:58 +0800

    压了一下图片，还有做了选牌动画

--- app/src/main/java/com/saveandstudio/mario/cdd/Scenes/Scene.java
+++ app/src/main/java/com/saveandstudio/mario/cdd/Scenes/Scene.java
@@ -2,7 +2,8 @@ package com.saveandstudio.mario.cdd.Scenes;
 
 import com.saveandstudio.mario.cdd.Components.AutoCollider;
 import com.saveandstudio.mario.cdd.Components.AutoPivot;
-import com.saveandstudio.mario.cdd.Components.TouchTest;
+import com.saveandstudio.mario.cdd.Components.TransformToTarget;
+import com.saveandstudio.mario.cdd.Components.TouchEvents;
 import com.saveandstudio.mario.cdd.GameBasic.*;
 import com.saveandstudio.mario.cdd.R;
 import com.saveandstudio.mario.cdd.Renderers.CardRenderer;
@@ -15,27 +16,30 @@ public class Scene {
     public static ArrayList<GameObject> gameObjectsList;
 
     public static void prePareScene() {
-        GameObject card = new GameObject(new Transform(new Vector3(GameViewInfo.centerW-60, GameViewInfo.centerH + 300, 2), 0,
-                new Vector3((float) 0.3, (float)0.3, 0), Vector3.zero));
+        GameObject card = new GameObject(new Transform(new Vector3(GameViewInfo.centerW-40, GameViewInfo.centerH + 300, 2), 0,
+                new Vector3((float) 1, (float)1, 0), Vector3.zero));
         card.addComponent(new CardRenderer(R.mipmap.diamond, R.mipmap.red_three));
         card.addComponent(new BoxCollider());
         card.addComponent(new AutoCollider());
         card.addComponent(new AutoPivot());
-        card.addComponent(new TouchTest());
+        card.addComponent(new TransformToTarget());
+        card.addComponent(new TouchEvents());
         GameObject card2 = new GameObject(new Transform(new Vector3(GameViewInfo.centerW, GameViewInfo.centerH + 300, 3), 0,
-                new Vector3((float) 0.3, (float)0.3, 0), Vector3.zero));
+                new Vector3((float) 1, (float)1, 0), Vector3.zero));
         card2.addComponent(new CardRenderer(R.mipmap.spade, R.mipmap.black_two));
         card2.addComponent(new BoxCollider());
         card2.addComponent(new AutoCollider());
         card2.addComponent(new AutoPivot());
-        card2.addComponent(new TouchTest());
-        GameObject card3 = new GameObject(new Transform(new Vector3(GameViewInfo.centerW + 60, GameViewInfo.centerH + 300, 4), 0,
-                new Vector3((float) 0.3, (float)0.3, 0), Vector3.zero));
+        card2.addComponent(new TransformToTarget());
+        card2.addComponent(new TouchEvents());
+        GameObject card3 = new GameObject(new Transform(new Vector3(GameViewInfo.centerW + 40, GameViewInfo.centerH + 300, 4), 0,
+                new Vector3((float) 1, (float)1, 0), Vector3.zero));
         card3.addComponent(new CardRenderer(R.mipmap.heart, R.mipmap.red_joker));
         card3.addComponent(new BoxCollider());
         card3.addComponent(new AutoCollider());
         card3.addComponent(new AutoPivot());
-        card3.addComponent(new TouchTest());
+        card3.addComponent(new TransformToTarget());
+        card3.addComponent(new TouchEvents());
     }
 
     public static void InstantiateStart() {
