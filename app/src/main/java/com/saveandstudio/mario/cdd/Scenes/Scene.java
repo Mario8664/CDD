abb14b1c9dd2e031e4f19e516e7f3401fffcdaed
Mario8664 <pooh3000@126.com>
2019-05-19 02:57:26 +0800

    取消了“新游戏”按钮，原因有二：第一涉及多线程难写，还容易崩。第二新游戏按钮要写也应属于游戏物件，而不是外部UI，可能日后会增加。 初步定了牌的大局。

--- app/src/main/java/com/saveandstudio/mario/cdd/Scenes/Scene.java
+++ app/src/main/java/com/saveandstudio/mario/cdd/Scenes/Scene.java
@@ -1,41 +1,27 @@
 package com.saveandstudio.mario.cdd.Scenes;
 
-import com.saveandstudio.mario.cdd.Components.AutoCollider;
-import com.saveandstudio.mario.cdd.Components.AutoPivot;
-import com.saveandstudio.mario.cdd.Components.TransformToTarget;
-import com.saveandstudio.mario.cdd.Components.TouchEvents;
+import com.saveandstudio.mario.cdd.Components.TouchCardEvents;
 import com.saveandstudio.mario.cdd.GameBasic.*;
 import com.saveandstudio.mario.cdd.Prefabs.Card;
-import com.saveandstudio.mario.cdd.R;
-import com.saveandstudio.mario.cdd.Renderers.CardRenderer;
+import com.saveandstudio.mario.cdd.Prefabs.Game;
+import com.saveandstudio.mario.cdd.Prefabs.Player;
 
 import java.util.ArrayList;
 
 public class Scene {
 
-    public static ArrayList<GameObject> instantiateList;
     public static ArrayList<GameObject> gameObjectsList;
 
     public static void prePareScene() {
-        Transform transform = new Transform(new Vector3(GameViewInfo.centerW, GameViewInfo.centerH + 300, 0), 0,
-                new Vector3((float) 1, (float)1, 0), Vector3.zero);
-        GameObject cardDesk = new GameObject(transform);
-        Card card = new Card(0, 3, new Transform(new Vector3(-40, 0, 2),0, Vector3.one,transform, Vector3.zero));
-        Card card2 = new Card(3, 12, new Transform(new Vector3(0, 0, 3),0, Vector3.one,transform, Vector3.zero));
-        Card card3 = new Card(2, 9, new Transform(new Vector3(40, 0, 4),0, Vector3.one,transform, Vector3.zero));
-        Card card4 = new Card(1, 7, new Transform(new Vector3(80, 0, 5),0, Vector3.one,transform, Vector3.zero));
-        Card card5 = new Card(2, 5, new Transform(new Vector3(120, 0, 6),0, Vector3.one,transform, Vector3.zero));
-    }
-
-    public static void InstantiateStart() {
-        if (instantiateList != null) {
-            for (int i = 0; i < instantiateList.size(); i++) {
-                GameObject tempGameObject = instantiateList.get(i);
-                tempGameObject.Start();
-                gameObjectsList.add(tempGameObject);
-                instantiateList.remove(tempGameObject);
-            }
-        }
+        Game game = new Game();
+        Player player = new Player(0, new Transform(new Vector3(GameViewInfo.centerW, GameViewInfo.centerH + 550, 0), 0,
+                Vector3.one), true);
+        Player Rplayer1 = new Player(1, new Transform(new Vector3(GameViewInfo.centerW + 400, GameViewInfo.centerH - 50, 0), 0,
+                Vector3.one), false);
+        Player Rplayer2 = new Player(0, new Transform(new Vector3(GameViewInfo.centerW, GameViewInfo.centerH - 650, 0), 0,
+                Vector3.one), false);
+        Player Rplayer3 = new Player(1, new Transform(new Vector3(GameViewInfo.centerW - 400, GameViewInfo.centerH - 50, 0), 0,
+                Vector3.one), false);
     }
 
     public static void Clear(){
